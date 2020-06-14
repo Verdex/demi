bfc0b79eafc4368e141f174780bd741f66e76aaa
David L. Inman <davidlevere@gmail.com>
2020-06-14 00:56:42 -0400

    incremental progress for top level

--- src/parsing/top_level.rs
+++ src/parsing/top_level.rs
@@ -6,6 +6,36 @@ use super::input::Input;
 impl<'a> Input<'a> {
 
     pub fn parse_top_level(&mut self) -> Result<TopLevel, ParseError> {
+         
+        // TODO maybe public?
+
+        Err(ParseError::EndOfFile("".to_string()))
+    }
+
+    fn parse_fun_def(&mut self) -> Result<FunDef, ParseError> {
+        
+        self.expect("fun")?;
+        
+        let name = self.parse_symbol()?;
+
+        self.expect("<")?; // TODO maybe
+
+        let type_params = self.list(|input| input.parse_symbol())?;
+
+        self.expect(">")?;
+
+
+        self.expect("(")?;
+
+        // parse parameters
+
+        // parse return type maybe
+
+        // parse { 
+
+        // parse statements or exprs
+
+
 
         Err(ParseError::EndOfFile("".to_string()))
     }
