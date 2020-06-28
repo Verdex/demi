95c1a6f843ee381f4fbd40a7efef53b961b0c6bf
David L. Inman <davidlevere@gmail.com>
2020-06-27 21:15:37 -0400

    lua gen stuff

--- src/code_gen/lua_gen.rs
+++ src/code_gen/lua_gen.rs
@@ -1,11 +1,34 @@
 
 use crate::parsing::ast::*;
-use super::code::Code;
 
-pub fn gen_lua( code : Code ) -> String { 
-    match code {
-        Code::S(s) => s,
-        _ => panic!("gen lua"),
+pub fn gen_lua( module : Mod ) -> String { 
+    module.fun_defs.into_iter().map(|def| gen_fun_def(def)).collect::<Vec<String>>().join("\n")
+}
+
+fn gen_fun_def( fun_def : FunDef ) -> String {
+    fn p( params : Vec<FunParam> ) -> String {
+        params.into_iter().map(|p| p.name.value).collect::<Vec<String>>().join(", ")
+    }
+    fn d( statements : Vec<Statement> ) -> String {
+        statements.into_iter().map(|s| gen_statement(s, 0)).collect::<Vec<String>>().join("\n")
+    }
+    format!(r#"
+function {}({})
+{}
+end"#, fun_def.name.value, p(fun_def.params), d(fun_def.definition))
+}
+
+fn gen_statement( statement : Statement, _indent : usize ) -> String {
+    match statement {
+        Statement::Return(Some(s)) => format!("return {}", gen_expr(s)),
+        Statement::Return(None) => format!("return"),
+        _ => panic!("TODO finish gen statement"),
     }
 }
 
+fn gen_expr( expr : Expr ) -> String {
+    match expr {
+        Expr::Number(s) => format!(" {} ", s.value ),
+        _ => panic!("TODO finish expr code gen"),
+    }
+}
